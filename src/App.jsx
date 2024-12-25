9e3184eb17f63d16e007afea5ce63b8c872d4bcf
GovindSingh <govindsingh988877@gmail.com>
2024-12-25 13:56:33 +0530

    yashhu bhai commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,11 @@ function App() {
   
   return (
     <>
-      <div className='bg-'>
+      <div className=''>
         <h1>hello</h1>
+        <div>
+          <h3 className='text-5xl'>Happy Birthday Yashhu Bhai</h3>
+        </div>
         </div>  
     </>
   )
